5536f623935be5157b0805bbfdf0219ca633be1d
Chao Ye <Chao12.ye@gmail.com>
2019-03-18 11:40:30 -0700

    add App component, FirstComponent, and NamedComponent

--- part-1/index.js
+++ part-1/index.js
@@ -1,6 +1,27 @@
+class App extends React.Component{
+    render(){
+        return (
+            <div>
+                <FirstComponent />
+                <NamedComponent name="Rithm10"/>
+            </div>
+        );    
+    }
+}
+
 class FirstComponent extends React.Component{
     render () {
-      return <h1>"My very first component."</h1>
+      return <h1>My very first component.</h1>
     }
   }
-  
+
+
+  class NamedComponent extends React.Component{
+      render () {
+          return <p> My name is {this.props.name}  </p>
+      }
+  }
+
+  ReactDOM.render(
+    <App/>, document.getElementById("root")
+  );
